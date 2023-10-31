d06a4c58980ddf70b4f63d4ec721ecd7e67e0692
devhasibulislam <devhasibulislam@gmail.com>
2023-10-31 19:00:57 +0600

    signin api integrated

---
+++ pages/api/signin.js
@@ -0,0 +1,20 @@
+import clientPromise from "@/lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("opedia-projectx");
+
+  switch (req.method) {
+    case "POST":
+      const bodyObject = req.body;
+      const user = await db
+        .collection("users")
+        .find({ email: bodyObject.email });
+
+      res.status(201).json({ success: user.acknowledged, message: "Created!" });
+      break;
+
+    default:
+      res.status(405).json({ error: "Method not allowed" });
+  }
+}
