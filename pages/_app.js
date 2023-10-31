a68bc26e8609701a79daeb0a932ca3d3c7a13426
devhasibulislam <devhasibulislam@gmail.com>
2023-10-31 17:26:00 +0600

    connected context api with app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,28 @@
 import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
+import { createContext, useState } from "react";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600"],
 });
 
+export const UserContext = createContext(null);
+
 export default function App({ Component, pageProps }) {
+  const [user, setUser] = useState({});
+  const userInformation = {
+    user,
+    setUser,
+  };
+
   return (
-    <main
-      className={`${poppins.className} h-screen w-screen overflow-x-hidden overflow-y-auto`}
-    >
-      <Component {...pageProps} />
-    </main>
+    <UserContext.Provider value={userInformation}>
+      <main
+        className={`${poppins.className} h-screen w-screen overflow-x-hidden overflow-y-auto`}
+      >
+        <Component {...pageProps} />
+      </main>
+    </UserContext.Provider>
   );
 }
